cece282c7b127134cd83099890a70b3578946b86
MigueUli <ulianovish@gmail.com>
2017-06-14 14:47:21 -0500

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+
+export default {
+    appName: 'musicapp',
+    apiKey: '5b2a4a83dc7fd8cedd12f4b8c990ca75',
+    secret: '6ec0db41172d9c42ac21c750d79c391f',
+    registeredTo: 'ulianovish'
+}
+// Application name	musicapp
+// API key	5b2a4a83dc7fd8cedd12f4b8c990ca75
+// Shared secret	6ec0db41172d9c42ac21c750d79c391f
+// Registered to	ulianovish
